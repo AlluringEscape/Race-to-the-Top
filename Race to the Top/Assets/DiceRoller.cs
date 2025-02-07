b8d9933132988358f30fac0bf3cc8b2958f72a84
AlluringEscape <n.j.linton35@gmail.com>
2025-02-06 23:55:54 -0600

    Implemented and tested Dice System with UI integration.

--- Race to the Top/Assets/DiceRoller.cs
+++ Race to the Top/Assets/DiceRoller.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DiceRoller : MonoBehaviour {
-    public Text diceResultsText;  // UI Text to display results
-    public int diceSides = 6; // Number of sides on the dice
+public class DiceRoller : MonoBehaviour
+{
+    public Text diceResultText;  // UI text to show the dice result
+    public int diceMin = 1;
+    public int diceMax = 6;
 
     public void RollDice()
     {
-        int roll = Random.Range(1, diceSides + 1);
-        diceResultsText.text = "Rolled: " + roll;
-        Debug.Log("Dice roll: " + roll);
+        int roll = Random.Range(diceMin, diceMax + 1);
+        diceResultText.text = "Rolled: " + roll;
+        Debug.Log("Player rolled: " + roll);
+        MovePlayer(roll);
+    }
+
+    void MovePlayer(int spaces)
+    {
+        // TODO: Move the player forward by "spaces" tiles.
+        Debug.Log("Move player forward by " + spaces + " spaces.");
     }
 }
